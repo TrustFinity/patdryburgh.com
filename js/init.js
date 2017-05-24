114d5c2bc2f41939c8a21ef253ec2c38bb319b45
Pat Dryburgh <pat@brewhouse.io>
2017-05-24 22:26:12 +0300

    added night theme

--- js/init.js
+++ js/init.js
@@ -1,9 +1,24 @@
+---
+---
+
 // lazy-load images
 echo.init({
   offset: 1000,
   unload: true,
-  debounce: false,
-  callback: function (element, op) {
-    console.log(element, 'has been', op + 'ed')
-  }
+  debounce: false
 });
+
+function setTheme() {
+  
+  var currentTime = new Date().getHours();
+  
+  if (0 <= currentTime&&currentTime < 8) {
+    document.write("<link rel='stylesheet' href='{{ "/css/night.css" | absolute_url }}' type='text/css'>");
+  }
+  if (8 <= currentTime&&currentTime < 20) {
+    document.write("<link rel='stylesheet' href='{{ "/css/main.css" | absolute_url }}' type='text/css'>");
+  }
+  if (20 <= currentTime&&currentTime <= 24) {
+    document.write("<link rel='stylesheet' href='{{ "/css/night.css" | absolute_url }}' type='text/css'>");
+  }
+}
